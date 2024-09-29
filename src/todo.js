9c8f3984f563a5aaa33643bf10eee39744d30ff2
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-09-29 19:22:31 +0530

    Add todo file and upload to remote

--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,6 @@
 function todo() {
     const todoPage = document.createElement('div');
     const newTodoContainer = document.createElement('div');
+    const newTodo = document.createElement('textarea')
+    const dueDate = document.createElement('input');
 }
